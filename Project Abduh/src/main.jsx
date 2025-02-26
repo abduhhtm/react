a3d5684c53ed32265dff0ad70424531483e1f881
abduhhtm <160695516+abduhhtm@users.noreply.github.com>
2025-02-26 09:43:58 +0700

    dw

--- Project Abduh/src/main.jsx
+++ Project Abduh/src/main.jsx
@@ -4,11 +4,12 @@
 // import { Skaye } from './m10/gambar1'
 // import {BookList} from './m14/coba'
 // import Nodepency from './m10/COBA'
-import { StatusBar } from './m11/ggg'
-import { ThemeStatus } from './m11/tugas/gambar1'
+// import { StatusBar } from './m11/ggg'
+// import { ThemeStatus } from './m11/tugas/gambar1'
+import { Axioshop18 } from './m18-Update & Delete a/gambar1/path'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeStatus/>
+    <Axioshop18/>
   </StrictMode>,
 )
